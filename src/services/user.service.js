aa65da07083e2094448ec9c960113b2fdad7174c
Prasansani-wijesundare <74582311+Prasansani-wijesundare@users.noreply.github.com>
2021-04-19 16:43:44 +0530

    First commit

---
+++ src/services/user.service.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import authHeader from "./auth-header";
+
+const API_URL = "http://apps.avantrio.xyz:8010/api/user/login";
+
+const getPublicContent = () => {
+  return axios.get(API_URL + "all");
+};
+
+const getUserBoard = () => {
+  return axios.get(API_URL + "user", { headers: authHeader() });
+};
+
+const getModeratorBoard = () => {
+  return axios.get(API_URL + "mod", { headers: authHeader() });
+};
+
+const getAdminBoard = () => {
+  return axios.get(API_URL + "admin", { headers: authHeader() });
+};
+
+export default {
+  getPublicContent,
+  getUserBoard,
+  getModeratorBoard,
+  getAdminBoard,
+};
